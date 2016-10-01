a92f9b99263ad9203c5f820fafb47706570df07a
jaapvaneggelen <jaapvaneggelen@gmail.com>
2016-10-01 16:49:42 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <h1>Welkom in de test</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <iframe width="800px" frameBorder="0" height="900px" src="https://www.verloonme.nu?bedr=NTB&email=jaapvaneggelen115@hotmail.com&sh=69b7dec3d768f864d7ee21ec3b5be66c3b1a4b9fa2929a897365cecdc17d79eb"></iframe>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
+    </body>
+</html>
